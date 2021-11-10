2a036cfab2739da9e5f44ae579867429c5874aa2
Joao Pedro <eng.joao.pedro@outlook.com>
2021-11-09 22:07:49 -0300

    separating routes

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
-const PORTA = 3000
+const PORT = 3000
+const path = require('path')
+const apiRouter = require('./routes/api')
+ 
+app.use('/api', apiRouter)
 
-app.listen(PORTA, () => console.log(`Server rodando na porta ${PORTA}`))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-let mural = [
-    {
-        id: 'fsdfsdf',
-        title: 'test title',
-        description: 'test description'
-    }
-]
-
-app.get('/all', (req, res) => {
-    res.json(JSON.stringify(mural))
-})
-
-app.post('/new',bodyParser.json(), (req, res) => {
-    let id = generateID()
-    let title = req.body.title
-    let description = req.body.description
-    
-    mural.push({id, title, description})
-
-    res.send('Post Adicionado')
-})
-
-function generateID(){
-    return Math.random().toString(36).substr(2, 9)
-}
-
+app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`))
